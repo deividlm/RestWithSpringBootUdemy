d6a18ee97b476c3b651faeaa7b6b46a4dbaeadd1
deivid <deividlm@hotmail.com>
2021-03-15 07:58:21 -0300

    Start project

---
+++ RestWithSpringBootUdemy/src/test/java/com/deividlm/reswithspringbootudemy/RestWithSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deividlm.reswithspringbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
